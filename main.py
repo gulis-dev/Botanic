666482ac9f3da97033c9155489b9d9abec744f3c
gulis-dev <gulispolska@gmail.com>
2025-01-18 12:52:37 +0100

    first test version

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QHBoxLayout
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEnginePage
+from PyQt6.QtCore import QUrl
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Browser(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Prosta Przeglądarka")
+        self.setGeometry(100, 100, 1200, 800)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+        # Centralny widget
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
 
+        # Layout
+        self.layout = QVBoxLayout(self.central_widget)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Pasek adresu
+        self.address_bar = QLineEdit(self)
+        self.address_bar.setPlaceholderText("Wpisz adres URL...")
+        self.address_bar.returnPressed.connect(self.navigate_to_url)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Przyciski na pasku
+        self.layout.addWidget(self.address_bar)
+
+        # Przeglądarka
+        self.browser = QWebEngineView(self)
+        self.layout.addWidget(self.browser)
+
+        # Pasek narzędzi
+        self.toolbar_layout = QHBoxLayout()
+        self.layout.addLayout(self.toolbar_layout)
+
+        self.reload_button = QPushButton("Odśwież", self)
+        self.reload_button.clicked.connect(self.reload_page)
+        self.toolbar_layout.addWidget(self.reload_button)
+
+        self.go_button = QPushButton("Przejdź", self)
+        self.go_button.clicked.connect(self.navigate_to_url)
+        self.toolbar_layout.addWidget(self.go_button)
+
+        # Ustawiamy początkowy URL
+        self.browser.setUrl(QUrl("https://www.google.com"))
+
+    def navigate_to_url(self):
+        url = self.address_bar.text()
+        if not url.startswith('http://') and not url.startswith('https://'):
+            url = 'https://' + url
+        self.browser.setUrl(QUrl(url))
+
+    def reload_page(self):
+        self.browser.reload()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    browser = Browser()
+    browser.show()
+    sys.exit(app.exec())
